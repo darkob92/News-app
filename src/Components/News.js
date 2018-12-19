4568d1a860837fdf414fcb2da14db561a5c8ceaf
Darko <darko.bogovic92@gmail.com>
2018-12-19 19:44:38 +0100

    reworking fetch and displaying data

--- src/Components/News.js
+++ src/Components/News.js
@@ -11,13 +11,17 @@ class News extends Component {
   componentDidMount() {
     fetch('https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=0664c5e916b443598676b05520c6fde6')
     .then(response => response.json())
-    .then(news => this.setState({news}, () => console.log('news fetched...', news)));
+    .then((news) => 
+      this.setState({news: news.articles}, () => console.log(news.articles)));
   }
 
   render() {
     return (
       <div>
         <h2 className='text-primary'>More News</h2>
+          {this.state.news.map(article => 
+            <p key={Math.floor(Math.random() * 1000)}>{ article.author } {article.title}</p>
+          )}
       </div>
     )
   }
